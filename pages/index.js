ddde76ef3cea27fc668d5b922e196efe2c79f281
Buuz135 <buuz135@hotmail.com>
2022-02-07 20:15:53 +0100

    Background

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,22 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+
+
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      
+    </div>
+  )
+}
 
-      <main>
+/*
+<main>
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
@@ -18,6 +26,4 @@ export default function Home() {
       </main>
 
       <Footer />
-    </div>
-  )
-}
+      */
